2727195b4b77b3dbf58dd60f7e2831bfbb30fea9
Sital Sitoula <heavywater221@gmail.com>
2022-02-23 13:43:43 -0600

    Json response using dictionary

--- imdbapi/watchlist_app/urls.py
+++ imdbapi/watchlist_app/urls.py
@@ -1,1 +1,6 @@
-from django.db import url
+from django.urls import path,include
+from watchlist_app.views import movie_list
+
+urlpatterns = [
+    path('list/', movie_list, name='movie-list')
+]
--- imdbapi/watchlist_app/views.py
+++ imdbapi/watchlist_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
 # Create your views here.
+def movie_list(request):
+    # Create a queryset
+    movies = Movie.objects.all()
+    data = {'movies': list(movies.values())}
+    
+    return JsonResponse(data)
+    # return in the form of JSON
+    
+   
