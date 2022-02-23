61ab4f8a12832c6d54eff500b2e65f4c3dcee333
Sital Sitoula <heavywater221@gmail.com>
2022-02-23 13:51:58 -0600

    manually creating json responses

--- imdbapi/watchlist_app/urls.py
+++ imdbapi/watchlist_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from watchlist_app.views import movie_list
+from watchlist_app.views import movie_list, movie_detail
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list')
+    path('list/', movie_list, name='movie-list'),
+    path('<int:pk>', movie_detail, name='movie-detail'),
 ]
