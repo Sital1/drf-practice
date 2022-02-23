8e7e8a88b7b731cd7a67ac92bb8ef2a347c6fdcb
Sital Sitoula <heavywater221@gmail.com>
2022-02-23 13:27:16 -0600

    Create movie model

---
+++ imdbapi/watchlist_app/models.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
