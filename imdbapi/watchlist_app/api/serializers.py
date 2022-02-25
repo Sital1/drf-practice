f8191f0f9d61d1f46b7ed7634e563257947ab021
Sital Sitoula <heavywater221@gmail.com>
2022-02-24 23:07:08 -0600

    use of serializer

--- imdbapi/watchlist_app/api/serializers.py
+++ imdbapi/watchlist_app/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from watchlist_app.models import Movie
+
 # using serializers Now
 # maps all the values
 class MovieSerializer(serializers.Serializer):
@@ -7,4 +9,22 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+    
+    # takes self and validated data
+    def create(self, validated_data):
+        # create an instance in the database
+        # The data has all the data
+        return Movie.objects.create(**validated_data)
+    
+    # update condition
+    # instance carries old value
+    # validated consist new value
+    def update(self, instance, validated_data):
+        # update each individual value
+        instance.name = validated_data.get("name", instance.name)
+        instance.description = validated_data.get("description", instance.description)
+        instance.active = validated_data.get("active", instance.active)
         
+        # save it
+        instance.save()
+        return instance
--- imdbapi/watchlist_app/api/views.py
+++ imdbapi/watchlist_app/api/views.py
@@ -1,4 +1,57 @@
-from watchlist_app.models import Movie
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from watchlist_app.models import Movie
+from watchlist_app.api.serializers import MovieSerializer
+
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    pass
+    if request.method == 'GET':
+        
+        # get all the data first
+        movies = Movie.objects.all()
+        
+        # utilize serializer. 
+        # Serializer needs to visit Multiple objects. So many=true
+        serializer = MovieSerializer(movies, many=True)
+        
+        # Send the data. Serializer is an object. Use .data to access the object
+        return Response(serializer.data)
+    
+    # Get data from user
+    if request.method == 'POST':
+        # request will have the data
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) # data is from the return in the create method in the serializer
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_details(request, pk):
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        # Put means update every single field. Rewrite everything.
+        # Someone is sending the data
+        # which object are we updating?
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        # fetch the data
+        movie = Movie.objects.get(pk=pk)
+        # delete it. is a queryset method
+        movie.delete()
+        
+        return Response()
